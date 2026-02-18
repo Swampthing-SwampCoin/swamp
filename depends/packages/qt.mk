4dd89e7a4d3a6ffffa77863bfaff6d1e9ad669b0
DavidR <wewehello@gmail.com>
2026-02-18 12:59:06 -0500

    Update qt.mk

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -129,6 +129,7 @@ $(package)_config_opts += -no-feature-undoview
 $(package)_config_opts += -no-feature-vnc
 $(package)_config_opts += -no-feature-wizard
 $(package)_config_opts += -no-feature-xml
+$(package)_config_opts += -no-feature-rhi
 
 $(package)_config_opts_darwin = -no-dbus
 $(package)_config_opts_darwin += -no-opengl
