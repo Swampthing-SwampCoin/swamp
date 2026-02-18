f0a5ec16d7648d86ecf4a66fdf7ac757955fd03e
DavidR <wewehello@gmail.com>
2026-02-18 14:51:19 -0500

    Update optionsmodel.cpp

--- src/qt/optionsmodel.cpp
+++ src/qt/optionsmodel.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2011-2015 The Bitcoin Core developers
-// Copyright (c) 2014-2018 The Dash Core developers 
+// Copyright (c) 2014-2018 The Dash Core developers
 // Copyright (c) 2018-2018 The Swamp Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
@@ -7,6 +7,8 @@
 #if defined(HAVE_CONFIG_H)
 #include "config/swamp-config.h"
 #endif
+
+#include <QNetworkProxy>
 
 #include "optionsmodel.h"
 
@@ -30,7 +32,6 @@
 
 #include <QSettings>
 #include <QStringList>
-#include <QtNetwork/QNetworkProxy>
 
 OptionsModel::OptionsModel(QObject *parent, bool resetSettings) :
     QAbstractListModel(parent)
